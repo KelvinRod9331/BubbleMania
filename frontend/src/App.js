3eb15aaafe866574769ceceaa21885821532b74e
romie151 <romiezelaya@ac.c4q.nyc>
2018-02-06 17:57:57 -0500

    div updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 id="title">BUBBLE MANIA</h1> 
-        <div id="game"></div>
-        <div id="store"></div>
+        <div id="banner"><h1 id="title">BUBBLE MANIA</h1><div id="signIn"><p id="sign">Sign-in/Register</p></div></div> 
+        <div id="game">
+        <div id="life"><p>Life Div</p></div>
+        <div id="stat"><p>Stat Div</p></div>
+        <div id="score"><p>Score Div</p></div>
+        <p>Game Div</p>
+        </div>
+        <div id="store"><h1>Store Div</h1></div>
+        <div id="topPlayer"><h1>Top Player Div</h1></div>
+        <div id="themeSelector"><h1>Color/Theme Div</h1></div>
       </div>
     );
   }
